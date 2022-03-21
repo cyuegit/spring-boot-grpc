7c0bf58d2f4b0feec3a96c362db4778b318a518a
cyuegit <910597566@qq.com>
2022-03-21 16:36:43 +0800

    grpc-loadbalancer SPI注入提取

--- spring-boot-grpc-client/src/main/java/com/xcbeyond/springboot/grpc/client/SpringBootGrpcClientApplication.java
+++ spring-boot-grpc-client/src/main/java/com/xcbeyond/springboot/grpc/client/SpringBootGrpcClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SpringBootGrpcClientApplication {
 
 	public static void main(String[] args) {
